8001c6e4b63cbf057c631913ac30739db3ff8bc1
Robert Mateus Moreira Gomes <robert.gomes.it@gmail.com>
2026-01-31 10:44:05 +0000

    finish `api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  // valida que é uma data ISO válida
+  expect(Number.isNaN(Date.parse(responseBody.update_at))).toBe(false);
+
+  expect(responseBody.database.version).toBe("16.0");
+  expect(responseBody.database.max_connections).toBe(100);
+  expect(responseBody.database.opened_connections).toBeGreaterThanOrEqual(1);
 });
